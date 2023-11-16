bb0c09e1488bb6509103bc2242b3eca1417594e7
Dev <dev@thecodizt.com>
2023-11-16 11:15:04 +0530

    ADD: Requirements

--- cross_sectional_data_generator.py
+++ cross_sectional_data_generator.py
@@ -1,28 +1,21 @@
-import pandas as pd
-import random
 from faker import Faker
-
-fake = Faker()
+import pandas as pd
+from categorical_data_generator import generate_categorical_data
+from numeric_data_generator import generate_numeric_data
+from geographical_data_generator import generate_geographical_data
 
 def generate_cross_sectional_data(num_samples, columns):
     data = {}
+
     for col in columns:
         col_name = col["name"]
         col_type = col["type"]
 
         if col_type == "Numeric":
-            min_val = col["min"]
-            max_val = col["max"]
-            data[col_name] = [random.uniform(min_val, max_val) for _ in range(num_samples)]
+            data[col_name] = generate_numeric_data(num_samples, col["min"], col["max"], col["mean"], col["std_deviation"])
         elif col_type == "Categorical":
-            data[col_name] = [fake.word() for _ in range(num_samples)]
-        elif col_type == "Date":
-            min_date = col["min"]
-            max_date = col["max"]
-            data[col_name] = [fake.date_time_between_dates(min_date, max_date) for _ in range(num_samples)]
+            data[col_name] = generate_categorical_data(num_samples, col["categories"])
         elif col_type == "Geographical":
-            data[col_name] = [fake.local_latlng(country_code='US', coords_only=True) for _ in range(num_samples)]
-        else:
-            raise ValueError(f"Unsupported column type: {col_type}")
+            data[col_name] = generate_geographical_data(num_samples)
 
     return pd.DataFrame(data)
